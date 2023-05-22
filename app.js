bcb8f06faafeb7044ffd0a7a20cf7855a53ba049
JeanMMillet <millet.jm@live.com>
2023-05-22 10:31:52 +0200

    put movies

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.get("/api/users/:id", usersHandlers.getUserById);
 
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", usersHandlers.postUser);
+
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
